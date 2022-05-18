e8325b879017a00bc6f98414dc1cebf67b4d095b
Veikko Hiltunen <veikko.hiltun@gmail.com>
2022-05-18 19:26:42 +0300

    Fix bottom navigation re-selecting and "up" gesture again

--- xml-test-app/app/src/main/java/com/example/testapplication/view/MainActivity.kt
+++ xml-test-app/app/src/main/java/com/example/testapplication/view/MainActivity.kt
@@ -3,17 +3,24 @@ package com.example.testapplication.view
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.get
 import androidx.navigation.ui.*
 import com.example.testapplication.R
 import com.example.testapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +29,46 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         onSupportNavigateUp()
 
         with(binding) {
             bottomNav.setupWithNavController(navController)
+            bottomNav.setOnItemReselectedListener(navController::popToRoot)
         }
     }
+
+    override fun onBackPressed() {
+        with(binding) {
+            bottomNav.run {
+                if (handle(navController)) {
+                    selectedItemId = menu[0].itemId
+                } else {
+                    super.onBackPressed()
+                }
+            }
+        }
+    }
+
+}
+
+private fun BottomNavigationView.handle(navController: NavController): Boolean {
+    val firstItemId = menu[0].itemId
+    // On the root, left it normally
+    if (selectedItemId == firstItemId) return false
+
+    val prevDes = navController.previousBackStackEntry?.destination ?: return false
+    val prevId = prevDes.parent?.id ?: prevDes.id
+
+    return prevId == firstItemId
+}
+
+private fun NavController.popToRoot(menuItem: MenuItem) {
+    val graph = graph[menuItem.itemId] as? NavGraph ?: return
+
+    popBackStack(
+        graph.startDestinationId,
+        false,
+    )
 }
